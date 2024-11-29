198a93fca1209a224eeb56cce7d837b2a429b59a
Kayis Rahman <kaisbk1@gmail.com>
2024-11-29 10:33:44 +0000

    Get Inbox Introduced

--- back-end/index.js
+++ back-end/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { Pool } = require('pg')
+
 const app = express()
 const port = 8080
 
@@ -6,6 +8,30 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/inbox', (req, res) => {
+  return getInbox(req, res)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// Repositories definition
+const pool = new Pool({
+  user: 'kayisrahman',
+  host: 'localhost',
+  database: 'gtd',
+  password: 'password',
+  port: 5432
+})
+
+const getInbox = (request, response) => {
+  pool.query('SELECT * FROM tasks ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
