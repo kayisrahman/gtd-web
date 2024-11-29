1823c3cc3603f85d2139b1b9ed1241a9097c7278
Kayis Rahman <kaisbk1@gmail.com>
2024-11-29 10:41:28 +0000

    Added body parser to fix json encoding

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const { Pool } = require('pg')
-const { create } = require('node:domain')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 8080
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
