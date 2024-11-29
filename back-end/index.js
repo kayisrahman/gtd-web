e2f14c97789a308d1b092e29f12b35b5ed8361d5
Kayis Rahman <kaisbk1@gmail.com>
2024-11-29 10:47:49 +0000

    Database layer moved to repo class

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const { Pool } = require('pg')
 const bodyParser = require('body-parser')
 
+const repo = require('./repository/repo')
 const app = express()
 const port = 8080
 
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: true
   })
 )
 
@@ -16,48 +16,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/task', (req, res) => {
-  return getInbox(req, res)
-})
-
-app.post('/tasks', (req, res) => {
-  return createTask(req, res)
-})
+// Routs
+app.get('/task', repo.getInbox)
+app.post('/tasks', repo.createTask)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
 
-
-// Repositories definition
-const pool = new Pool({
-  user: 'kayisrahman',
-  host: 'localhost',
-  database: 'gtd',
-  password: 'password',
-  port: 5432
-})
-
-const getInbox = (request, response) => {
-  pool.query('SELECT * FROM tasks ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const createTask = (request, response) => {
-  console.log(request)
-  const { title, notes, date, time, context, priority } = request.body
-
-  pool.query('INSERT INTO tasks (title, notes, date, time, context, priority) ' +
-    'VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-    [title, notes, date, time, context, priority],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`Task added added with ID: ${results.rows[0].id}`)
-    })
-}
--- back-end/repository/repo.js
+++ back-end/repository/repo.js
@@ -1,5 +1,38 @@
 const Pool = require('pg').Pool
 
+const pool = new Pool({
+  user: 'kayisrahman',
+  host: 'localhost',
+  database: 'gtd',
+  password: 'password',
+  port: 5432
+})
 
+const getInbox = (request, response) => {
+  pool.query('SELECT * FROM tasks ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
-export
+const createTask = (request, response) => {
+  console.log(request)
+  const { title, notes, date, time, context, priority } = request.body
+
+  pool.query('INSERT INTO tasks (title, notes, date, time, context_id, priority) ' +
+    'VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+    [title, notes, date, time, context, priority],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Task added added with ID: ${results.rows[0].id}`)
+    })
+}
+
+module.exports = {
+  getInbox,
+  createTask
+}
