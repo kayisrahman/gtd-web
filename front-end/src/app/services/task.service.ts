b29026f5b30424e3400b0a54061829c46bd50972
Kayis Rahman <kaisbk1@gmail.com>
2024-12-06 12:26:22 +0000

    Check in missing services,

---
+++ front-end/src/app/services/task.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core'
+import { environment } from '../../environments/environment'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Task } from '../model/task'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private static readonly REST_API_URI = environment.restApi + environment.configContextPath + 'tasks'
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Array<Task>> {
+    return this.http.get<Array<Task>>(TaskService.REST_API_URI)
+  }
+}
