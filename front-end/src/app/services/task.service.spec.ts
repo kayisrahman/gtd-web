1e7dea7a8cc582472b503846b4a02c8b47e48de7
Kayis Rahman <kaisbk1@gmail.com>
2024-12-12 13:39:35 +0000

    should fetch all tasks from the inbox added

--- front-end/src/app/services/task.service.spec.ts
+++ front-end/src/app/services/task.service.spec.ts
@@ -1,16 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { TaskService } from './task.service';
+import { TaskService } from './task.service'
+import { TaskFilter } from '../model/TaskFilter'
+import { Task } from '../model/Task'
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing'
+import { environment } from '../../environments/environment'
+
 
 describe('TaskService', () => {
+  const REST_API_URI = environment.restApi + environment.configContextPath + 'tasks'
+
   let service: TaskService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TaskService],
+    });
+
     service = TestBed.inject(TaskService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-});
+
+  // it('should fetch all tasks with filters', () => {
+  //   const mockTasks: Task[] = [
+  //     { 'id': 1, title: 'Task 1', status: 'Todo' },
+  //     { 'id': 2, title: 'Task 2', status: 'Done' }
+  //   ]
+  //
+  //   const filter: TaskFilter = { status: 'Todo' }
+  //
+  //   service.getAll(filter).subscribe((tasks) => {
+  //     expect(tasks).toEqual(mockTasks)
+  //   })
+  //
+  //   const req = httpMock.expectOne((request) =>
+  //     request.url === REST_API_URI && request.method === 'GET'
+  //   )
+  //   expect(req.request.params.get('status')).toBe('Todo')
+  //
+  //   req.flush(mockTasks)
+  // })
+
+})
